6ab8398fe3fdc8e380101689590260cac7ad86e5
Emmylong1 <98855013+Emmylong1@users.noreply.github.com>
2023-02-27 03:07:01 +0000

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAWGFHVGZ3HX5GCBKI"
-  secret_key = "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"
+  access_key = "ACCESS_KEY"
+  secret_key = "SECRET_KEY"
 }
 
 variable "subnet_prefix" {
